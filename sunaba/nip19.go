1237eb292d06d99cc01697bb37850cb2c719c7af
foxytanuki <biz@foxytanuki.dev>
2023-02-11 04:33:57 +0900

    update sunaba/nip19.go

--- sunaba/nip19.go
+++ sunaba/nip19.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	"github.com/nbd-wtf/go-nostr"
 	"github.com/nbd-wtf/go-nostr/nip19"
 )
 
@@ -17,4 +18,18 @@ func main() {
 			fmt.Println(s)
 		}
 	}
+	fmt.Println("---")
+
+	priv := "647d4c14c560a1d2a1abeeb11d340a9dd3a08fa53e5736f1e4cafc78309a575e"
+	if bech32priv, err := nip19.EncodePrivateKey(priv); err == nil {
+		fmt.Println(bech32priv)
+		fmt.Println(priv)
+		if pub, err := nostr.GetPublicKey(priv); err == nil {
+			if bech32pub, err := nip19.EncodePublicKey(pub); err == nil {
+				fmt.Println(bech32pub)
+				fmt.Println(pub)
+			}
+		}
+	}
+	fmt.Println("---")
 }
